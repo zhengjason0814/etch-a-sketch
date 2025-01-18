0d9d467f11e988eabe32041faffb914ee4eabeb0
Jason Zheng <jasonzheng0814@gmail.com>
2025-01-18 16:39:41 -0500

    add button function to recreate different grids

--- functionality.js
+++ functionality.js
@@ -1,4 +1,5 @@
 const squareContainer = document.querySelector(".container");
+const btn = document.querySelector("button");
 
 for (let i = 0; i < 16; i++) {
     for (let j = 0; j < 16; j++) {
@@ -12,10 +13,34 @@ for (let i = 0; i < 16; i++) {
 }
 
 squareContainer.addEventListener("mouseover", changeColor);
+btn.addEventListener("click", generateNewGrid);
 
 
 function changeColor(event) {
     if (event.target.matches(".container div")) {
         event.target.style.backgroundColor = "red";
+    }
+}
+
+function generateNewGrid() {
+    let input = parseInt(prompt("Please enter a number between 0 - 100!"));
+    if (input < 0) input = 0;
+    else if (input > 100) input = 100;
+
+    let squares = squareContainer.querySelectorAll("div");
+    squares.forEach((square) => square.remove());
+
+    for (let i = 0; i < input; i++) {
+        for (let j = 0; j < input; j++) {
+            square = document.createElement("div");
+
+            changedRatio = (450 - input * 3) / input;
+            newRatio = changedRatio + "px";
+
+            square.style.width = newRatio;
+            square.style.height = newRatio;
+            square.style.backgroundColor = "black";
+            squareContainer.appendChild(square);
+        }
     }
 }
